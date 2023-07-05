06acde89fb5cc7bdaed5009b052c9ebc92db493f
JayroGT <jayro.esp.sg@gmail.com>
2023-07-04 20:47:22 -0500

    correccion

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="flex items-center justify-center h-full">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<TasksList />} />
+            <Route path="/react-rtk-crud" element={<TasksList />} />
             <Route path="/create-task" element={<TaskForm />} />
             <Route path="/edit-task/:id" element={<TaskForm />} />
           </Routes>
